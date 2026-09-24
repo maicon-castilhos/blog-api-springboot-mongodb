58eb667fc62fa39454a037e606e7812984724bca
Maicon Amaral <maiktrader54@gmail.com>
2026-09-24 15:40:53 -0300

    feat: obtendo post por id

--- src/main/java/com/maiconamaral/workshopmongo/services/PostService.java
+++ src/main/java/com/maiconamaral/workshopmongo/services/PostService.java
@@ -1,12 +1,14 @@
 package com.maiconamaral.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maiconamaral.workshopmongo.domain.Post;
 import com.maiconamaral.workshopmongo.repository.PostRepository;
+import com.maiconamaral.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
@@ -16,5 +18,10 @@ public class PostService {
 
 	public List<Post> findByAuthor(String id) {
 		return repo.findByAuthorId(id);
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
