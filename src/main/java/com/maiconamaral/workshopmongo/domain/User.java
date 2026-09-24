741067314aa5bf41780f1943cfbbc73b715fabe0
Maicon Amaral <maiktrader54@gmail.com>
2026-09-24 14:30:55 -0300

    Referenciando posts do usuário

--- src/main/java/com/maiconamaral/workshopmongo/domain/User.java
+++ src/main/java/com/maiconamaral/workshopmongo/domain/User.java
@@ -1,65 +1,78 @@
 package com.maiconamaral.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	private String id;
-	private String name;
-	private String email;
+ private static final long serialVersionUID = 1L;
+ 
+ @Id
+ private String id;
+ private String name;
+ private String email;
+ @DBRef(lazy = true)
+ private List<Post> posts = new ArrayList<>();
 
-	public User() {
-	}
+ public User() {
+ }
 
-	public User(String id, String name, String email) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-	}
+ public User(String id, String name, String email) {
+ this.id = id;
+ this.name = name;
+ this.email = email;
+ }
 
-	public String getId() {
-		return id;
-	}
+ public String getId() {
+ return id;
+ }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+ public void setId(String id) {
+ this.id = id;
+ }
 
-	public String getName() {
-		return name;
-	}
+ public String getName() {
+ return name;
+ }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+ public void setName(String name) {
+ this.name = name;
+ }
 
-	public String getEmail() {
-		return email;
-	}
+ public String getEmail() {
+ return email;
+ }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+ public void setEmail(String email) {
+ this.email = email;
+ }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		User user = (User) o;
-		return Objects.equals(id, user.id);
-	}
+ public List<Post> getPosts() {
+ return posts;
+ }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+ public void setPosts(List<Post> posts) {
+ this.posts = posts;
+ }
+
+ @Override
+ public boolean equals(Object o) {
+ if (this == o)
+ return true;
+ if (o == null || getClass() != o.getClass())
+ return false;
+ User user = (User) o;
+ return Objects.equals(id, user.id);
+ }
+
+ @Override
+ public int hashCode() {
+ return Objects.hash(id);
+ }
 }
